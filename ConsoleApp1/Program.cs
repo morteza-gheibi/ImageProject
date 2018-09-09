5eb50332818d7dff35d04b6c25ac2768926fca03
morteza.gheibi <morteza.gheibi@gmail.com>
2018-09-09 15:39:19 +0430

    remove comment from console

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,26 +12,11 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-
-            var uow = new GenericUoW();
-
-            //uow.Repository<Work>().Insert(new Work { WorkTitle = "woek test" });
-            //uow.SaveChanges();
+        {   
             string base64Guid = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
             var uid = Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "");
-
             var ss = uid.Length;
             var s = base64Guid.Length;
-           //var e= uow.Repository<Category>().GetAll();
-
-            //var tb = new Category() { CategoryId = uid, CategoryName = "test", ParentId = 0 ,CreateDate=DateTime.Now , CreateUserId=1,IsActive=true,IsDelete=true };
-            //uow.Repository<Category>().Insert(tb);
-            //uow.SaveChanges();
-
-
-
-
         }
     }
 }
